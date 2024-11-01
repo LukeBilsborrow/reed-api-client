014b39f3f8e293e61c97b97f045dc10b863646db
LukeBilsborrow <lukebilsborrow@gmail.com>
2024-11-01 18:16:02 +0000

    updated tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,37 @@
 import pytest
+import _model
 import client
 import utils
+import dotenv
+import os
+
+pytest_plugins = ("pytest_asyncio",)
+dotenv.load_dotenv()
+TOKEN = os.getenv("REED_API_KEY")
 
 
-@pytest.mark.parametrize("use_https", [True, False])
-def test_client_default_url(use_https):
-    protocol = "https" if use_https else "http"
-    _client = client.ReedApiClient("", use_https=use_https)
-    assert _client.base_url == utils.get_base_url(protocol=protocol)
+def test_client_default_url():
+    _client = client.ReedApiClient(TOKEN)
+    assert _client.base_url == utils.get_base_url(protocol="https")
+
+
+def test_client_custom_url():
+    _client = client.ReedApiClient(TOKEN, override_full_url="https://example.com")
+    assert _client.base_url == "https://example.com"
+
+
+def test_job_search():
+    _client = client.ReedApiClient(TOKEN)
+    result = _client.job_search(locationName="London", resultsToTake=1, use_async=False)
+
+    assert type(result) == _model.JobSearchResponse
+
+
+@pytest.mark.asyncio
+async def test_job_search_async():
+    _client = client.ReedApiClient(TOKEN)
+    result = _client.job_search(locationName="London", resultsToTake=1, use_async=True)
+    result = await result
+    print(result)
+
+    assert type(result) == _model.JobSearchResponse
