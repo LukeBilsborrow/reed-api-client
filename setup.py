fbaaaf3304fd95744c754b3e97ff6ad4755b7b54
LukeBilsborrow <lukebilsborrow@gmail.com>
2024-11-19 17:57:54 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
 from setuptools import setup
 
-setup(
-    name="reedjobs",
-    version="0.1.0",
-    author="LukeBilsborrow",
-    description="A Python client for the Reed Jobseeker API.",
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/LukeBilsborrow/reed-api-client",
-    packages=["reedjobs"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.8",
-    install_requires=[
-        "httpx",
-        "pydantic",
-    ],
-)
+setup(name="reedjobs",
+      version="0.1.0",
+      author="LukeBilsborrow",
+      description="A Python client for the Reed Jobseeker API.",
+      long_description=open("README.md", "r", encoding="utf-8").read(),
+      long_description_content_type="text/markdown",
+      url="https://github.com/LukeBilsborrow/reed-api-client",
+      packages=["reedjobs"],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires=">=3.8")
